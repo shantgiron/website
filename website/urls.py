f10a3a46a88a4fda68c587c541e18c46790b69dd
shantgiron <shantgiron@gmail.com>
2017-10-19 23:33:52 -0400

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,21 +1,42 @@
-"""untitled3 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
-from django.contrib import admin
+from app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+
+    #login page
+    url(r'^$', views.index1, name='index1'),
+    # Home page
+    url(r'^$', views.index, name='index'),
+
+
+    # Formulario de cliente
+    url(r'^$', views.form_cliente, name='form_cliente'),
+    # Modificar cliente
+    url(r'^$', views.modificar_cliente, name='modificar_cliente'),
+
+
+    # Formulario de prestamo
+    url(r'^$', views.form_prestamo, name='form_prestamo'),
+    # Modificar prestamo
+    url(r'^$', views.modificar_prestamo, name='modificar_prestamo'),
+    # Calculo de prestamos
+    url(r'^$', views.calculo_de_prestamos, name='calculo_de_prestamos'),
+
+
+
+    # Formulario de usuario
+    url(r'^$', views.form_usuario, name='form_usuario'),
+
+
+    #Listados
+    # Recibos al cobro
+    url(r'^$', views.recibos_al_cobro, name='recibos_al_cobro'),
+    # Recibos despues del cobro
+    url(r'^$', views.recibos_despues_del_cobro, name='recibos_despues_del_cobro'),
+    # Estados de cuenta
+    url(r'^$', views.estados de cuenta, name='estados_de_cuenta'),
+    # Historial de cuenta
+    url(r'^$', views.historial_de_cuenta, name='historial_de_cuenta'),
+    # Relacion de ingresos
+    url(r'^$', views.relacion_de_ingresos, name='relacion_de_ingresos'),
 ]
